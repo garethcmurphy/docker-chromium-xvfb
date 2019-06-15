0365efd8c6a44ae29380ca9be9664ed18418a82e
Gareth Murphy <garethcmurphy@gmail.com>
2019-06-15 06:47:03 +0200

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -21,6 +21,9 @@ docker build -f images/js-onbuild/Dockerfile-8.x -t markadams/chromium-xvfb-js:8
 docker build -f images/js/Dockerfile-9.x -t markadams/chromium-xvfb-js:9 images/js
 docker build -f images/js-onbuild/Dockerfile-9.x -t markadams/chromium-xvfb-js:9-onbuild images/js-onbuild
 
+docker build -f images/js/Dockerfile-12.x -t markadams/chromium-xvfb-js:12 images/js
+docker build -f images/js-onbuild/Dockerfile-12.x -t markadams/chromium-xvfb-js:12-onbuild images/js-onbuild
+
 
 docker build -f samples/js/Dockerfile -t markadams/chromium-xvfb-js-sample samples/js
 
